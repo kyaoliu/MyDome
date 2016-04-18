fdfa19ec89b9c65a1487ed6a162027ee1da4c8a6
kyaoliu <kyaoliu@163.com>
2016-04-18 10:41:49 +0800

    添加 webapi2 注册webapi2路由 ： 注意路由规则要先匹配api

---
+++ Src/Web/Controllers/DefaultController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Web.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            var model = new Models.TestModel()
+            {
+                Age = 11,
+                Id = 111,
+                UserName = "ddddd"
+            };
+            return Json(model);
+        }
+
+        //public Models.TestModel Get(int id)
+        //{
+        //    var model = new Models.TestModel()
+        //    {
+        //        Age = 11,
+        //        Id = id,
+        //        UserName = "ddddd"
+        //    };
+
+        //    return model;
+        //}
+
+        public Models.TestModel Get(int id,string name)
+        {
+            var model = new Models.TestModel()
+            {
+                Age = 11,
+                Id = id,
+                UserName = name
+            };
+
+            return model;
+        }
+        [HttpGet]
+        public Models.TestModel Test(int id)
+        {
+            var model = new Models.TestModel()
+            {
+                Age = 11,
+                Id = id,
+                UserName = "Test"
+            };
+
+            return model;
+        }
+    }
+}
